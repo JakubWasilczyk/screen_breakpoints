e923358d5d5de8c9c573cf0ef80e99328130ae74
Jakub Wasilczyk <jakub.wasilczyk@alias2k.com>
2022-09-20 12:12:09 +0200

    Added example

---
+++ example/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:screen_breakpoints/screen_breakpoints.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: BreakpointContainer(
+          fluid: true,
+          alignment: Alignment.center,
+          xlChild: Text('${widget.title} - Desktop'),
+          mdChild: Text('${widget.title} - Tablet'),
+          child: Text('${widget.title} - Mobile'),
+        ),
+      ),
+      body: BreakpointContainer(
+        xlChild: _DesktopView(counter: _counter),
+        mdChild: _TabletView(counter: _counter),
+        child: _MobileView(counter: _counter),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _MobileView extends StatelessWidget {
+  final int counter;
+
+  const _MobileView({Key? key, required this.counter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _TabletView extends StatelessWidget {
+  final int counter;
+
+  const _TabletView({Key? key, required this.counter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DesktopView extends StatelessWidget {
+  final int counter;
+
+  const _DesktopView({Key? key, required this.counter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
